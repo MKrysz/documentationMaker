864bf0da7baaceda17388ae32fa859c659445d95
MACIEJ KRYSZTOWIAK <mkrysz@student.agh.edu.pl>
2021-04-23 14:24:42 +0200

    read description of pdfs from .txt files

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import os
 import webbrowser
-htmlName = "documentation.html"
 
+# change name of created html file and directory to your documentation
+htmlName = "documentation.html"
+dir = "C:/Users/macie/Documents/Documentation"
+
+
+# creates .html file and writes basic html template
 def startFile():
     file = open(htmlName, 'w')
     file.write("<!DOCTYPE html>\n")
@@ -13,23 +18,37 @@ def startFile():
     file.write("<ul>\n")
     return file
 
+
+# writes basic template and closes .html file
 def closeFile():
-    file.write("\n")
+    file.write("</ul>\n")
     file.write("</body>\n")
     file.write("</html>\n")
 
+#starts html list with 'name' header
 def startList(name):
     file.write("<p></p> <li><ul><h><b>" + name + "</b></h>\n")
 
+#closes list
 def closeList():
     file.write("</ul></li>\n")
 
+#adds pdf file to a list
 def addItem(directory, doc):
-    buffer = "<li><a href=\"" + directory + '/' + doc + "\" target = \"_blank\">"+ doc[:-4]
-    if os.name == "nt":
-        buffer = buffer.replace('/', '\\')
+    buffer = "<li><a href=\"" + getAddress(directory, doc) + "\" target = \"_blank\">" + doc[:-4]
     file.write(buffer + "</a></li>\n")
 
+def getAddress(directory, doc):
+    result = directory + '/' + doc
+    if os.name == "nt":
+        result = result.replace('/', '\\')
+    return result
+
+def addDescripion(txtFile):
+    file.write(str(txtFile.read()))
+    file.write("<p></p>\n")
+
+#main recursive function
 def rek(dir):
     result = list(os.walk(dir))[0]
     if result[1]:
@@ -43,9 +62,12 @@ def rek(dir):
         for doc in result[2]:
             if doc[-3:] == "pdf":
                 addItem(result[0], doc)
+                for txtFile in result[2]:
+                    if txtFile == doc[:-4] + ".txt":
+                        description = open(getAddress(result[0], txtFile), 'r')
+                        addDescripion(description)
 
 
-dir = "C:/Users/macie/Documents/Documentation"
 gen = list(os.walk(dir))
 file = startFile()
 rek(dir)
