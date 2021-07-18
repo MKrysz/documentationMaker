04e54a79e9831571ba57c6d595af255b98f5a14d
MACIEJ KRYSZTOWIAK <mkrysz@student.agh.edu.pl>
2021-07-18 20:35:06 +0200

    Started working on DocEditor.py

--- DocEditor.py
+++ DocEditor.py
@@ -1,2 +1,14 @@
 import os
 import DocSettings
+
+print("Choose path to file or type /s 'keyword' to search for all files containing that keyword:")
+i = 0
+for setting in DocSettings.settings:
+    print('[', i, '] ', setting[2])
+    i += 1
+userInput = input()
+if userInput.isdigit:
+    choice = int(userInput)
+    result = DocSettings.settings[choice][2]
+result = list(os.walk(result))[0][1]
+print(result)
